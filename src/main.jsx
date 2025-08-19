d252e69e390d1e2bab6314e560a75733d24ba261
NateHigger <NateHigger@Agartha.com>
2025-08-19 09:16:59 +0700

    lol

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Optional performance monitoring
+// Optional: Web Vitals
 if (process.env.NODE_ENV === 'production') {
   import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
     getCLS(console.log)
@@ -16,5 +16,5 @@ if (process.env.NODE_ENV === 'production') {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
